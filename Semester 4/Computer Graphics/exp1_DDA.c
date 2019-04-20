c6813a16b0c87d2a73df058ad7a17ede5e73bb88
Danish Ali Ansari <35758526+ansaridanishali@users.noreply.github.com>
2019-04-20 12:05:57 +0530

    Update exp1_DDA.c

--- Semester 4/Computer Graphics/exp1_DDA.c
+++ Semester 4/Computer Graphics/exp1_DDA.c
@@ -11,6 +11,7 @@ void main(){
     scanf("%f%f%f%f\n",&x1, &y1, &x2, &y2);
     dx = (int)(x2 - x1);
     dy = (int)(y2 - y1);
+
     if (dx >= dy)
         step = dx;
     else
@@ -30,5 +31,4 @@ void main(){
     }
     getch();
     closegraph();
-
 }
